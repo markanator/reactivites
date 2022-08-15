970d7de956bf1a9c43adc8dcded98ae896b8bc6a
Mark Ambro <wowmarkambro@gmail.com>
2022-08-14 23:42:37 -0600

    fix cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,6 +27,11 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://127.0.0.1:5173");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +47,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
