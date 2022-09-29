23c11655b8abd67a14ce2a480f2f13a3aa88a82a
Mark Ambro <wowmarkambro@gmail.com>
2022-09-29 02:25:11 -0600

    SEO and footer

--- client-app/src/app/ActivitiesLayout.tsx
+++ client-app/src/app/ActivitiesLayout.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, Link, Text, useColorModeValue } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
@@ -6,9 +6,27 @@ function App() {
   return (
     <Flex flexDir="column">
       <Navbar />
-      <Box bg={useColorModeValue("gray.50", "gray.800")} h="full">
+      <Box bg={useColorModeValue("gray.50", "gray.800")} h="full" pb={8}>
         <Outlet />
       </Box>
+      <Flex
+        as="footer"
+        justifyContent="center"
+        bg={useColorModeValue("gray.200", "gray.800")}
+        alignItems="center"
+        py={4}
+      >
+        <Text>
+          Developed with 🧠 by:{" "}
+          <Link
+            fontWeight={600}
+            href="https://twitter.com/_mark_ambro"
+            target="_blank"
+          >
+            Mark Ambro
+          </Link>
+        </Text>
+      </Flex>
     </Flex>
   );
 }
