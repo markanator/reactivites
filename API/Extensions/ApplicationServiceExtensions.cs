bc9ae9498b8fe0e39de9354238a38ab25b6309e8
Mark Ambro <wowmarkambro@gmail.com>
2022-10-03 01:11:02 -0600

    S16: setup cloudinary integration

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Activities;
 using Application.Core;
 using Application.Interfaces;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -38,6 +39,10 @@ namespace API.Extensions
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
             // add ability to get user from httpContext from any authenticated reqs
             services.AddScoped<IUserAccessor, UserAccessor>();
+            // Dep Inj
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            // add cloud photo config
+            services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
             return services;
         }
---
+++ Application/Interfaces/IPhotoAccessor.cs
@@ -0,0 +1,11 @@
+﻿using Application.Photos;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Interfaces
+{
+    public interface IPhotoAccessor
+    {
+        Task<PhotoUploadResults> AddPhoto(IFormFile file);
+        Task<string> DeletePhoto(string publicId);
+    }
+}
