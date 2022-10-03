d9c3e2b2fb6f9594234f4afd3abdcafe635e66db
Mark Ambro <wowmarkambro@gmail.com>
2022-10-03 01:10:04 -0600

    add Photos w/ migrations

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -10,6 +10,7 @@ namespace Persistence
 
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> Attendees { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
