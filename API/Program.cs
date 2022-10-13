44a00092ce3b50266c2c479a4c89e30141d649a9
Mark Ambro <wowmarkambro@gmail.com>
2022-10-13 03:29:25 -0600

    fix docker build

--- API/Program.cs
+++ API/Program.cs
@@ -5,39 +5,39 @@ using Persistence;
 
 namespace API
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var host = CreateHostBuilder(args).Build();
+			AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 
-            using var scope = host.Services.CreateScope(); // will be disposed off
+			using var scope = host.Services.CreateScope(); // will be disposed off
 
-            var services = scope.ServiceProvider;
-            try
-            {
-                var context = services.GetRequiredService<DataContext>();
-                var userManager = services.GetRequiredService<UserManager<AppUser>>();
-                await context.Database.MigrateAsync();
-                // seed
-                await Seed.SeedData(context, userManager);
-            }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger>();
-                logger.LogError("Error ocurred during migration ::: " + ex.Message);
+			var services = scope.ServiceProvider;
+			try
+			{
+				var context = services.GetRequiredService<DataContext>();
+				var userManager = services.GetRequiredService<UserManager<AppUser>>();
+				await context.Database.MigrateAsync();
+				// seed
+				await Seed.SeedData(context, userManager);
+			}
+			catch (Exception ex)
+			{
+				var logger = services.GetRequiredService<ILogger<Program>>();
+				logger.LogError("Error ocurred during migration ::: " + ex.Message);
 
-            }
-            await host.RunAsync();
-        }
+			}
+			await host.RunAsync();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+				Host.CreateDefaultBuilder(args)
+						.ConfigureWebHostDefaults(webBuilder =>
+						{
+							webBuilder.UseStartup<Startup>();
+						});
+	}
 }
