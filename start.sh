e62e9b97a10e112624fa805ba4e0e8bc58dcdcd0
Mark Ambro <wowmarkambro@gmail.com>
2022-10-16 00:26:31 -0600

    fix: only run localCI when pushing to main

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+protected_branch='main'
+current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+
 set -ex
-npm run build:client
-flyctl deploy
+localCi(){
+	npm run build:client
+	flyctl deploy
+}
+
+if  [ $current_branch = $protected_branch ]; then
+  localCi
+fi
+
+unset localCi
+
+exit 0
